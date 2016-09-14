f976202e3c158db6634518013b54a761fc67659a
K K <kalkotekedar@gmail.com>
2016-09-14 22:02:38 +0530

    Initial Commit

--- app/src/main/java/com/example/kalkotekedar/ticktactoe/TTTHomeScreen.java
+++ app/src/main/java/com/example/kalkotekedar/ticktactoe/TTTHomeScreen.java
@@ -1,7 +1,9 @@
 package com.example.kalkotekedar.ticktactoe;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class TTTHomeScreen extends AppCompatActivity {
 
@@ -9,5 +11,13 @@ public class TTTHomeScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ttthome_screen);
+    }
+
+    public void startSinglePlayer(View view) {
+        startActivity(new Intent(getApplicationContext(), TTTOnePlayer.class));
+    }
+
+    public void startTwoPlayer(View view) {
+        startActivity(new Intent(getApplicationContext(), TTTTwoPlayer.class));
     }
 }
--- app/src/main/java/com/example/kalkotekedar/ticktactoe/TTTOnePlayer.java
+++ app/src/main/java/com/example/kalkotekedar/ticktactoe/TTTOnePlayer.java
@@ -1,7 +1,9 @@
 package com.example.kalkotekedar.ticktactoe;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class TTTOnePlayer extends AppCompatActivity {
 
