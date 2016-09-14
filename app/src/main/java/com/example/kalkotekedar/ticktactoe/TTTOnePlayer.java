26a184740581e12a92c6876f77feef940359e40a
K K <kalkotekedar@gmail.com>
2016-09-14 21:29:17 +0530

    Initial Commit

---
+++ app/src/main/java/com/example/kalkotekedar/ticktactoe/TTTOnePlayer.java
@@ -0,0 +1,13 @@
+package com.example.kalkotekedar.ticktactoe;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TTTOnePlayer extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tttone_player);
+    }
+}
